d1ba601396948edc1bd5bf3e4c6847a435ce57e3
Jay Convertino <johnathan.convertino.1@us.af.mil>
2024-05-16 17:55:46 -0400

    updated zcu102 boot build to function

--- src/uboot_scr/zynqmp/BOOT.cmd
+++ src/uboot_scr/zynqmp/BOOT.cmd
@@ -3,5 +3,5 @@ setenv kernel_load_address 0x3000000
 setenv devicetree_image devicetree.dtb
 setenv devicetree_load_address 0x2A00000
 
-setenv bootargs 'bootargs=earlycon=cdns,mmio,0xFF000000,115200n8 console=ttyPS0,115200n8 root=/dev/mmcblk0p2 rw rootwait cma=128M'
+setenv bootargs 'console=ttyPS0,115200 root=/dev/mmcblk0p2 rw earlycon rootfstype=ext4 rootwait clk_ignore_unused cpuidle.off=1'
 setenv mmc_boot 'echo Starting Linux... && fatload mmc 0 ${kernel_load_address} ${kernel_image} && fatload mmc 0 ${devicetree_load_address} ${devicetree_image} && booti ${kernel_load_address} - ${devicetree_load_address}'
