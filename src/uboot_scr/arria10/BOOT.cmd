d39481de799af5ec66ae3c230ba28ac7664ba2f2
Jay Convertino <johnathan.convertino.1@us.af.mil>
2024-05-16 20:46:26 -0400

    updated arria10 bootargs to correct tty

--- src/uboot_scr/arria10/BOOT.cmd
+++ src/uboot_scr/arria10/BOOT.cmd
@@ -3,5 +3,5 @@ setenv kernel_load_address 0x3000000
 setenv devicetree_image devicetree.dtb
 setenv devicetree_load_address 0x2A00000
 
-setenv bootargs 'console=ttyPS0,115200 root=/dev/mmcblk0p2 rw earlycon rootfstype=ext4 rootwait'
+setenv bootargs 'root=/dev/mmcblk0p2 rw rootwait earlyprintk console=ttyS0,115200n8'
 setenv mmc_boot 'echo Starting Linux... && fatload mmc 0 ${kernel_load_address} ${kernel_image} && fatload mmc 0 ${devicetree_load_address} ${devicetree_image} && bootz ${kernel_load_address} - ${devicetree_load_address}'
