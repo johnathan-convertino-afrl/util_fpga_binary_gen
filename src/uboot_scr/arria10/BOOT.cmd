128e49dc87984591e838fc4e037104933bfa947e
Jay Convertino <johnathan.convertino.1@us.af.mil>
2024-05-16 22:36:11 -0400

    fix arria10 boot.scr

--- src/uboot_scr/arria10/BOOT.cmd
+++ src/uboot_scr/arria10/BOOT.cmd
@@ -3,5 +3,6 @@ setenv kernel_load_address 0x1000000
 setenv devicetree_image devicetree.dtb
 setenv devicetree_load_address 0x2000000
 
-setenv bootargs 'root=/dev/mmcblk0p2 rw rootwait earlyprintk console=ttyS0,115200n8'
+setenv bootargs 'console=ttyS0,115200 root=/dev/mmcblk0p2 rw rootwait earlyprintk'
 setenv mmc_boot 'echo Starting Linux... && fatload mmc 0 ${kernel_load_address} ${kernel_image} && fatload mmc 0 ${devicetree_load_address} ${devicetree_image} && bootz ${kernel_load_address} - ${devicetree_load_address}'
+run mmc_boot
