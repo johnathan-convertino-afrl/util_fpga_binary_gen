ebed5a15141d4992711df1188f391ad5621d84f7
Jay Convertino <johnathan.convertino.1@us.af.mil>
2024-05-16 21:26:32 -0400

    update uboot addresses

--- src/uboot_scr/arria10/BOOT.cmd
+++ src/uboot_scr/arria10/BOOT.cmd
@@ -1,7 +1,7 @@
 setenv kernel_image zImage
-setenv kernel_load_address 0x3000000
+setenv kernel_load_address 0x1000000
 setenv devicetree_image devicetree.dtb
-setenv devicetree_load_address 0x2A00000
+setenv devicetree_load_address 0x2000000
 
 setenv bootargs 'root=/dev/mmcblk0p2 rw rootwait earlyprintk console=ttyS0,115200n8'
 setenv mmc_boot 'echo Starting Linux... && fatload mmc 0 ${kernel_load_address} ${kernel_image} && fatload mmc 0 ${devicetree_load_address} ${devicetree_image} && bootz ${kernel_load_address} - ${devicetree_load_address}'
