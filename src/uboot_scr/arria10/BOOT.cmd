becaffb724c64a64b4702f2ea251ed7fdf6a9e42
Jay Convertino <johnathan.convertino.1@us.af.mil>
2024-05-21 10:31:03 -0400

    add bridge enable

--- src/uboot_scr/arria10/BOOT.cmd
+++ src/uboot_scr/arria10/BOOT.cmd
@@ -5,4 +5,5 @@ setenv devicetree_load_address 0x2000000
 
 setenv bootargs 'console=ttyS0,115200 root=/dev/mmcblk0p2 rw rootwait earlyprintk'
 setenv mmc_boot 'echo Starting Linux... && fatload mmc 0 ${kernel_load_address} ${kernel_image} && fatload mmc 0 ${devicetree_load_address} ${devicetree_image} && bootz ${kernel_load_address} - ${devicetree_load_address}'
+bridge enable
 run mmc_boot
