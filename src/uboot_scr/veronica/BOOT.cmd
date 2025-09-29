af2c72ea8bc25a6ea58be94e7c10c84ff24daffb
Jay Convertino <johnathan.convertino.1@us.af.mil>
2025-09-29 10:01:02 -0400

    update boot command for uart lite

--- src/uboot_scr/veronica/BOOT.cmd
+++ src/uboot_scr/veronica/BOOT.cmd
@@ -3,5 +3,5 @@ setenv kernel_load_address 0x800C0000
 setenv devicetree_image devicetree.dtb
 setenv devicetree_load_address 0x80080000
 
-setenv bootargs 'console=ttyPS0,115200 root=/dev/mmcblk0p2 rw earlycon rootfstype=ext4 rootwait'
+setenv bootargs 'console=ttyUL0,115200 root=/dev/mmcblk0p2 rw earlycon rootfstype=ext4 rootwait'
 setenv mmc_boot 'echo Starting Linux... && fatload mmc 0 ${kernel_load_address} ${kernel_image} && fatload mmc 0 ${devicetree_load_address} ${devicetree_image} && bootm ${kernel_load_address} - ${devicetree_load_address}'
