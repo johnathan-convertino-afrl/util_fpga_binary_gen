d17ecc0e424ddc34b3590ea16afcfa09658b6086
Jay Convertino <johnathan.convertino.1@us.af.mil>
2025-09-30 21:19:40 -0400

    change boot command so it uses the compressed image file and then unzips it.

--- src/uboot_scr/veronica/BOOT.cmd
+++ src/uboot_scr/veronica/BOOT.cmd
@@ -1,7 +1,9 @@
-setenv kernel_image uImage
-setenv kernel_load_address 0x800C0000
+setenv kernel_image Image
+setenv kernel_load_address 0x80400000
+setenv kernel_gz_image Image.gz
+setenv kernel_gz_load_address 0x82000000
 setenv devicetree_image devicetree.dtb
-setenv devicetree_load_address 0x80080000
+setenv devicetree_load_address 0x87783000
 
 setenv bootargs 'console=ttyUL0,115200 root=/dev/mmcblk0p2 rw earlycon rootfstype=ext4 rootwait'
-setenv mmc_boot 'echo Starting Linux... && fatload mmc 0 ${kernel_load_address} ${kernel_image} && fatload mmc 0 ${devicetree_load_address} ${devicetree_image} && bootm ${kernel_load_address} - ${devicetree_load_address}'
+setenv mmc_boot 'echo Loading Linux... && fatload mmc 0 ${kernel_gz_load_address} ${kernel_gz_image} && unzip ${kernel_gz_load_address} ${kernel_load_address} && fatload mmc 0 ${devicetree_load_address} ${devicetree_image} && booti ${kernel_load_address} - ${devicetree_load_address}'
