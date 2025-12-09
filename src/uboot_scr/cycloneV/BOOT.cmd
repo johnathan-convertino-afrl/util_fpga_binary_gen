06b282cf0f70a22c9cc5e43d93ede8e3f3d0944e
Jay Convertino <electrobs@gmail.com>
2025-12-08 20:37:36 -0500

    add rough draft of cyclonev fpga gen, could use itb like the A10

--- src/uboot_scr/cycloneV/BOOT.cmd
+++
@@ -1,9 +0,0 @@
-setenv kernel_image zImage
-setenv kernel_load_address 0x1000000
-setenv devicetree_image devicetree.dtb
-setenv devicetree_load_address 0x2000000
-
-setenv bootargs 'console=ttyS0,115200 root=/dev/mmcblk0p2 rw rootwait earlyprintk'
-setenv mmc_boot 'echo Starting Linux... && fatload mmc 0 ${kernel_load_address} ${kernel_image} && fatload mmc 0 ${devicetree_load_address} ${devicetree_image} && bootz ${kernel_load_address} - ${devicetree_load_address}'
-bridge enable
-run mmc_boot
