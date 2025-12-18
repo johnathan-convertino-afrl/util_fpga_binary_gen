88766302dcc1bcc0e435cca3736d318bea5bb3c9
Jay Convertino <johnathan.convertino.1@us.af.mil>
2025-12-17 19:11:22 -0500

    added MCS file gen for programming of BPI memory in Vivado

--- src/tcl/create_mcs.tcl
+++ src/tcl/create_mcs.tcl
@@ -1,13 +1,17 @@
 # glob xpr, use it to find and open the project, export xsa
 
 set project [glob *.xpr]
+set bitfile [glob *.bit]
+
+set isize [lindex $argv 0]
+set iface [lindex $argv 1] 
 
 open_project $project
 
-#parameterize set my_mem_device [lindex [get_cfgmem_parts {mt25ql128-spi-x1_x2_x4}] 0], interface, bit file stuffs. compression?
-#     set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
-#     set_property BITSTREAM.CONFIG.CONFIGRATE 33 [current_design]
-#     set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]
-write_cfgmem  -format mcs -size 16 -interface SPIx4 -loadbit {up 0x00000000 [get_property DIRECTORY [get_projects]]/*.bit } -file [get_property DIRECTORY [get_projects]]/system_bit.mcs
+set root_dir [get_property DIRECTORY [get_projects]]
+
+write_cfgmem  -format mcs -size $isize -force -interface $iface -loadbit "up 0x00000000 $root_dir/$bitfile" -file $root_dir/system_bit.mcs
+
+set_property PROGRAM.FILES $root_dir/system_bit.mcs
 
 exit
