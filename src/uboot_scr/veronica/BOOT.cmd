f7e2afbad1efecdb888015dad537b30a94f5481d
Jay Convertino <johnathan.convertino.1@us.af.mil>
2026-01-08 20:59:16 -0500

    update boot veronica for better file setup

--- src/uboot_scr/veronica/BOOT.cmd
+++ src/uboot_scr/veronica/BOOT.cmd
@@ -3,10 +3,12 @@ setenv kernel_load_address 0x80400000
 setenv kernel_gz_image Image.gz
 setenv kernel_gz_load_address 0x82000000
 setenv devicetree_image devicetree.dtb
-setenv devicetree_load_address 0x83000000
+setenv devicetree_load_address 0x87400000
 setenv rootfs_image rootfs.cpio.uboot
-setenv rootfs_image_load_address 0x84000000
+setenv rootfs_image_load_address 0x87401000
+setenv fdt_high 0xffffffff
+setenv initrd_high 0x88000000
 
-setenv bootargs 'console=ttyUL0,115200 earlycon root=/dev/ram0 rw'
+setenv bootargs 'console=ttyUL0,115200 earlycon'
 
 setenv mmc_boot 'echo Loading Linux... && fatload mmc 0 ${kernel_gz_load_address} ${kernel_gz_image} && unzip ${kernel_gz_load_address} ${kernel_load_address} && echo Loading Devicetree... && fatload mmc 0 ${devicetree_load_address} ${devicetree_image} && echo Loading File System... && fatload mmc 0 ${rootfs_image_load_address} ${rootfs_image} && booti ${kernel_load_address} ${rootfs_image_load_address}:${filesize} ${devicetree_load_address}'
--- src/uboot_scr/veronica/ROOTFS_BOOT.cmd
+++ src/uboot_scr/veronica/ROOTFS_BOOT.cmd
@@ -3,7 +3,7 @@ setenv kernel_load_address 0x80400000
 setenv kernel_gz_image Image.gz
 setenv kernel_gz_load_address 0x82000000
 setenv devicetree_image devicetree.dtb
-setenv devicetree_load_address 0x87783000
+setenv devicetree_load_address 0x83000000
 
 setenv bootargs 'console=ttyUL0,115200 root=/dev/mmcblk0p2 rw earlycon rootfstype=ext4 rootwait'
 setenv mmc_boot 'echo Loading Linux... && fatload mmc 0 ${kernel_gz_load_address} ${kernel_gz_image} && unzip ${kernel_gz_load_address} ${kernel_load_address} && fatload mmc 0 ${devicetree_load_address} ${devicetree_image} && booti ${kernel_load_address} - ${devicetree_load_address}'
